f92da62cac8d1ee7d2e74a043fcdc54d367cb377
Pavan Pawar <pavanrpawar36@gmail.com>
2026-03-15 12:27:20 +0530

    Data Types

--- JavaScript/JavaScript Part 1/script.js
+++ JavaScript/JavaScript Part 1/script.js
@@ -6,3 +6,8 @@ console.log(a);
 let name = "Pavan";
 console.log(name);
 
+// Data Types
+// Primitive types - Number, Boolean, String, Undefined, Null, Bigint, Symbol
+console.log(typeof(a));
+console.log(typeof(name));
+
