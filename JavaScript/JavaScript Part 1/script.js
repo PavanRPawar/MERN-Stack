e8e1c3272560dbde47ea6284923bb29cfad13204
Pavan Pawar <pavanrpawar36@gmail.com>
2026-03-15 12:59:18 +0530

    Practice Questions

--- JavaScript/JavaScript Part 1/script.js
+++ JavaScript/JavaScript Part 1/script.js
@@ -46,3 +46,13 @@ let cgpa;
 cgpa = 8.43;
 const pi = 3.14;
 var number = 151;
+
+// Practice Questions
+age = 23
+age + 2
+console.log(age);
+let hindi = 80;
+let eng = 90;
+let math = 100;
+let avg = (hindi + eng + math) / 3;
+console.log(avg);
