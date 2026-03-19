fbe9addd83126399327e36adee14a39cc7d2772e
Pavan Pawar <pavanrpawar36@gmail.com>
2026-03-19 11:26:07 +0530

    Identifier

--- JavaScript/JavaScript Part 1/script.js
+++ JavaScript/JavaScript Part 1/script.js
@@ -73,3 +73,5 @@ age--       // age -= 1
 let num = 5;                // num = 5
 let newNum = num++;         // newNum = 5, num = 6
 newNum = ++num;            // newNum = 7, num = 7
+
+// Identifier 
